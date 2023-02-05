a6692ba7e7b0d2da7aef0c4c145002ea6f1928b2
jp-jp-123 <jpbeltran098@gmail.com>
2023-02-05 10:39:57 +0800

    Cleaned the code a little bit

--- NBodyV2/mainmenu.py
+++ NBodyV2/mainmenu.py
@@ -2,45 +2,49 @@ import pygame
 import os
 import button
 import sys
+# button and sys import, remove if unnecessary
 
-## creating the window
-WIDTH,HEIGHT = 1280,720
-WIN = pygame.display.set_mode((WIDTH,HEIGHT))
-pygame.display.set_caption("Main Menu") ## for the caption on top of window
+# creating the window
+WIDTH, HEIGHT = 1280, 720
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Main Menu")  # for the caption on top of window
 
-WHITE = (255,255,255) ## creating variable to easy call colors
+WHITE = (255, 255, 255)  # creating variable to easy call colors
+
 # gatherings pictures
-BACKGROUND = pygame.image.load(os.path.join('menu','backgroundpic.png'))
-TITLE = pygame.image.load(os.path.join('menu','texts','title.png'))
-START = pygame.image.load(os.path.join('menu','texts','start.png'))
-OPTIONS = pygame.image.load(os.path.join('menu','texts','options.png'))
-EXIT = pygame.image.load(os.path.join('menu','texts','exit.png'))
-BORDER = pygame.image.load(os.path.join('menu','texts','border.png'))
+BACKGROUND = pygame.image.load(os.path.join('menu', 'backgroundpic.png'))
+TITLE = pygame.image.load(os.path.join('menu', 'texts', 'title.png'))
+START = pygame.image.load(os.path.join('menu', 'texts', 'start.png'))
+OPTIONS = pygame.image.load(os.path.join('menu', 'texts', 'options.png'))
+EXIT = pygame.image.load(os.path.join('menu', 'texts', 'exit.png'))
+BORDER = pygame.image.load(os.path.join('menu', 'texts', 'border.png'))
 FPS = 80
 
-##button class
-class Button():
-    def __init__(self,x,y,image,scale):
-        width = image.get_width()
-        height = image.get_height()
+
+# Button class
+# (self.width, self.height, sense (hover()) and scale parameter is not used. Check if unnecessary and remove)
+class Button:
+    def __init__(self, x, y, image, scale):
+        self.width = image.get_width()
+        self.height = image.get_height()
         self.image = image
         self.rect = self.image.get_rect()
-        self.rect.topleft = (x,y)
-        self.clicked = False # var. prevents multiple click register 
+        self.rect.topleft = (x, y)
+        self.clicked = False  # var. prevents multiple click register
 
     def draw(self):
         action = False
-        #mouse position
+        # mouse position
         pos = pygame.mouse.get_pos()
-        #print(pos)
+        # print(pos)
 
         # mouse on button
         if self.rect.collidepoint(pos):
             WIN.blit(BORDER, (self.rect.x, self.rect.y))
-            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False: # prevents multiple click register
+            if pygame.mouse.get_pressed()[0] == 1 and not self.clicked:  # prevents multiple click register
                 self.clicked = True
                 action = True
-        #allows buttons to be re-clicked
+        # allows buttons to be re-clicked
         if pygame.mouse.get_pressed()[0] == 0:
             self.clicked = False
 
@@ -48,7 +52,7 @@ class Button():
         WIN.blit(self.image, (self.rect.x, self.rect.y))
 
         return action
-    
+
     def hover(self):
         sense = False
         position = pygame.mouse.get_pos()
@@ -59,40 +63,39 @@ class Button():
             print('HOVER')
 
 
-
 # button instances
-start_button = Button (540,280, START,1)
-options_button = Button (540,380, OPTIONS,1)
-exit_button = Button (540,480,EXIT, 1)
+start_button = Button(540, 280, START, 1)
+options_button = Button(540, 380, OPTIONS, 1)
+exit_button = Button(540, 480, EXIT, 1)
 
-def main():
+
+def main_window():
     clock = pygame.time.Clock()
     run = True
     while run:
-        ## background
-        WIN.blit(BACKGROUND,(0,0))
-        WIN.blit(TITLE,(0,0))
+        # background
+        WIN.blit(BACKGROUND, (0, 0))
+        WIN.blit(TITLE, (0, 0))
 
         # Buttons
-        if start_button.draw() == True:
+        if start_button.draw():
             print('START')
-        if options_button.draw() == True:
+        if options_button.draw():
             print('OPTIONS')
         if exit_button.draw():
             run = False
 
-        ## loop for the game whether to quit
-        clock.tick(FPS) ## sets the fps 
+        # loop for the game whether to quit
+
+        # sets the fps
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
         pygame.display.update()
-
-       
-       
-
     pygame.quit()
 
-if __name__=="__main__":
-    main()
+
+if __name__ == "__main__":
+    main_window()
